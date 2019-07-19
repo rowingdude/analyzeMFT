a053955932d636969c68a0da8594f96206d2d790
Edd Salkield <esalkield@deloitte.co.uk>
2019-07-19 13:11:50 +0100

    Updated readme and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+import setuptools
 from distutils.core import setup
 
 setup(
-    name='analyzeMFT',
-    version='2.0.19',
+    name='analyzeMFT3',
+    version='3.0.0',
     author='David Kovar',
     author_email='dkovar@gmail.com',
     packages=['analyzemft'],
-    url='http://github.com/dkovar/analyzeMFT',
+    url='http://github.com/eddsalkield/analyzeMFT3',
     license='LICENSE.txt',
     description='Analyze the $MFT from a NTFS filesystem.',
     long_description=open('README.txt').read(),

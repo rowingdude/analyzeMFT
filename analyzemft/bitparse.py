a789827bc7155d21a6f2b938c86ee344235984ba
Michał Walkowski <mi.walkowski@gmail.com>
2024-06-26 21:00:28 +0200

    Inform the user about the invalid buffer

--- analyzemft/bitparse.py
+++ analyzemft/bitparse.py
@@ -26,17 +26,19 @@ def parse_little_endian_signed_negative(buf):
     return ret
 
 
-def parse_little_endian_signed(buf):
-  
-  if not buf:
-      raise ValueError("Empty buffer")
+def parse_little_endian_signed(buf, size=4):
+    if not buf:
+        raise ValueError("Empty buffer")
 
-  value = 0
-  size = len(buf)
-  for i in range(size):
-      value |= buf[i] << (i * 8)
+    value = 0
+    if len(buf) != size:
+        print(F'Invalid buffer size: {len(buf)}')
+        size = len(buf)
 
-  if value & (1 << ((size * 8) - 1)):
-      value = - (value + (1 << size * 8))
+    for i in range(size):
+        value |= buf[i] << (i * 8)
 
-  return value
+    if value & (1 << ((size * 8) - 1)):
+        value = - (value + (1 << size * 8))
+
+    return value

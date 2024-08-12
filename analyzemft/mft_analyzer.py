29b2bc217eb5c2c7519e6625402477cc4f5aa2e1
Benjamin Cance <49796265+rowingdude@users.noreply.github.com>
2024-08-12 12:17:00 -0400

    update syntax to `self.xyz` python class structure.

--- analyzemft/mft_analyzer.py
+++ analyzemft/mft_analyzer.py
@@ -9,11 +9,14 @@ from typing import Dict, Any
 from .mftutils import WindowsTime
 
 class MFTAnalyzer:
-    def __init__(self, options):
+    def __init__(self, options: Any):
         self.options = options
-        self.mft = {}
-        self.folders = {}
-        self.num_records = 0
+        self.mft: Dict[int, Dict[str, Any]] = {}
+        self.folders: Dict[str, str] = {}
+        self.num_records: int = 0
+        self.attribute_handlers: Dict[int, Callable] = {
+            # To do: add handlers 
+        }
 
     def process_mft_file(self, file_mft):
         self.num_records = 0
@@ -29,25 +32,24 @@ class MFTAnalyzer:
 
         self.gen_filepaths()
 
-
-    def parse_record(raw_record: bytes, options: Any) -> Dict[str, Any]:
+    def parse_record(self, raw_record: bytes) -> Dict[str, Any]:
         record: Dict[str, Any] = {'filename': '', 'notes': '', 'fncnt': 0}
 
-        decodeMFTHeader(record, raw_record)
+        self.decodeMFTHeader(record, raw_record)
 
         record_number = record['recordnum']
 
-        if options.debug:
+        if self.options.debug:
             print(f"-->Record number: {record_number}\n\tMagic: {record['magic']} Attribute offset: {record['attr_off']} Flags: {hex(int(record['flags']))} Size:{record['size']}")
 
         if record['magic'] == 0x44414142:
-            if options.debug:
+            if self.options.debug:
                 print("BAAD MFT Record")
             record['baad'] = True
             return record
 
         if record['magic'] != 0x454c4946:
-            if options.debug:
+            if self.options.debug:
                 print("Corrupt MFT Record")
             record['corrupt'] = True
             return record
@@ -55,58 +57,60 @@ class MFTAnalyzer:
         read_ptr = record['attr_off']
 
         while read_ptr < 1024:
-            ATRrecord = decodeATRHeader(raw_record[read_ptr:])
+            ATRrecord = self.decodeATRHeader(raw_record[read_ptr:])
             if ATRrecord['type'] == 0xffffffff:  
                 break
 
-            if options.debug:
+            if self.options.debug:
                 print(f"Attribute type: {ATRrecord['type']:x} Length: {ATRrecord['len']} Res: {ATRrecord['res']:x}")
 
-            handler = attribute_handlers.get(ATRrecord['type'], handle_unknown_attribute)
-            handler(ATRrecord, raw_record[read_ptr:], record, options)
+            handler = self.attribute_handlers.get(ATRrecord['type'], self.handle_unknown_attribute)
+            handler(ATRrecord, raw_record[read_ptr:], record)
 
             if ATRrecord['len'] > 0:
                 read_ptr += ATRrecord['len']
             else:
-                if options.debug:
+                if self.options.debug:
                     print("ATRrecord->len <= 0, exiting loop")
                 break
 
-            pass
+        return record
 
-        def gen_filepaths(self):
-            for i in self.mft:
-                if self.mft[i]['filename'] == '':
-                    if self.mft[i]['fncnt'] > 0:
-                        self.get_folder_path(i)
-                        if self.options.debug:
-                            print(f"Filename (with path): {self.mft[i]['filename']}")
-                    else:
-                        self.mft[i]['filename'] = 'NoFNRecord'
+    def gen_filepaths(self):
+        for i in self.mft:
+            if self.mft[i]['filename'] == '':
+                if self.mft[i]['fncnt'] > 0:
+                    self.get_folder_path(i)
+                    if self.options.debug:
+                        print(f"Filename (with path): {self.mft[i]['filename']}")
+                else:
+                    self.mft[i]['filename'] = 'NoFNRecord'
 
-     def get_folder_path(self, seqnum):
-          if self.debug: print  ("Building Folder For Record Number (%d)" % seqnum)
+    def get_folder_path(self, seqnum: int) -> str:
+        if self.options.debug:
+            print(f"Building Folder For Record Number ({seqnum})")
 
-          if seqnum not in self.mft:
-               return 'Orphan'
+        if seqnum not in self.mft:
+            return 'Orphan'
 
-          if (self.mft[seqnum]['filename']) != '':
-               return self.mft[seqnum]['filename']
+        if self.mft[seqnum]['filename'] != '':
+            return self.mft[seqnum]['filename']
 
-          try:
-               if (self.mft[seqnum]['fn',0]['par_ref'] == 5): # Seq number 5 is "/", root of the directory
-                    self.mft[seqnum]['filename'] = '/' + self.mft[seqnum]['fn',self.mft[seqnum]['fncnt']-1]['name']
-                    return self.mft[seqnum]['filename']
-          except: 
-               self.mft[seqnum]['filename'] = 'NoFNRecord'
-               return self.mft[seqnum]['filename']
+        try:
+            if self.mft[seqnum]['fn', 0]['par_ref'] == 5:  # Seq number 5 is "/", root of the directory
+                self.mft[seqnum]['filename'] = '/' + self.mft[seqnum]['fn', self.mft[seqnum]['fncnt']-1]['name']
+                return self.mft[seqnum]['filename']
+        except:
+            self.mft[seqnum]['filename'] = 'NoFNRecord'
+            return self.mft[seqnum]['filename']
 
-          if (self.mft[seqnum]['fn',0]['par_ref']) == seqnum:
-               if self.debug: print  ("Error, self-referential, while trying to determine path for seqnum %s" % seqnum)
-               self.mft[seqnum]['filename'] = 'ORPHAN/' + self.mft[seqnum]['fn',self.mft[seqnum]['fncnt']-1]['name']
-               return self.mft[seqnum]['filename']
+        if self.mft[seqnum]['fn', 0]['par_ref'] == seqnum:
+            if self.options.debug:
+                print(f"Error, self-referential, while trying to determine path for seqnum {seqnum}")
+            self.mft[seqnum]['filename'] = 'ORPHAN/' + self.mft[seqnum]['fn', self.mft[seqnum]['fncnt']-1]['name']
+            return self.mft[seqnum]['filename']
 
-          parentpath = self.get_folder_path((self.mft[seqnum]['fn',0]['par_ref']))
-          self.mft[seqnum]['filename'] =  parentpath + '/' + self.mft[seqnum]['fn',self.mft[seqnum]['fncnt']-1]['name']
+        parentpath = self.get_folder_path(self.mft[seqnum]['fn', 0]['par_ref'])
+        self.mft[seqnum]['filename'] = parentpath + '/' + self.mft[seqnum]['fn', self.mft[seqnum]['fncnt']-1]['name']
 
-          return self.mft[seqnum]['filename']
+        return self.mft[seqnum]['filename']

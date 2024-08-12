04f33eb7224d7abcff894b9f34943c4a1da6a789
Benjamin Cance <49796265+rowingdude@users.noreply.github.com>
2024-08-12 14:20:42 -0400

    Update mftsessions to use new conventions and function references in the mft_formatters and mft_analyzer files.

--- analyzemft/mftsession.py
+++ analyzemft/mftsession.py
@@ -8,173 +8,85 @@
 # 2-Aug-24 
 # - Updating to current PEP
 
-VERSION='2.0.2'
+VERSION='2.1.1'
 
 import sys
-import ctypes
-import re
-import time
-import unicodedata
 import csv
-import os
-import platform
-from optparse import OptionParser
-import mft
-
-from mftutils import WindowsTime
-
-SIAttributeSizeXP = 72
-SIAttributeSizeNT = 48
-
+import logging
+from pathlib import Path
+from typing import TextIO
+from .mft_analyzer import MFTAnalyzer
+from .mft_formatters import mft_to_csv, mft_to_body, mft_to_l2t
 
 class MftSession:
-     'Class to describe an entire MFT processing session'
+    def __init__(self):
+        self.mft = {}
+        self.folders = {}
+        self.options = None
+        self.file_mft: TextIO = None
+        self.file_csv = None
+        self.file_body: TextIO = None
+        self.file_csv_time: TextIO = None
+        self.setup_logging()
 
-     def __init__(self):
-          self.mft = {}
-          self.folders = {}
-          self.debug = False
-          
-     def mft_options(self):
-     
-         parser = OptionParser()
-         parser.set_defaults(debug=False,UseLocalTimezone=False,UseGUI=False)
-         
-         parser.add_option("-v", "--version", action="store_true", dest="version",
-                           help="report version and exit")
-         
-         parser.add_option("-f", "--file", dest="filename",
-                           help="read MFT from FILE", metavar="FILE")
-         
-         parser.add_option("-o", "--output", dest="output",
-                           help="write results to FILE", metavar="FILE")
-         
-         parser.add_option("-a", "--anomaly",
-                           action="store_true", dest="anomaly",
-                           help="turn on anomaly detection")
-         
-         parser.add_option("-b", "--bodyfile", dest="bodyfile",
-                           help="write MAC information to bodyfile", metavar="FILE")
-         
-         parser.add_option("--bodystd", action="store_true", dest="bodystd",
-                           help="Use STD_INFO timestamps for body file rather than FN timestamps")
-         
-         parser.add_option("--bodyfull", action="store_true", dest="bodyfull",
-                           help="Use full path name + filename rather than just filename")
-         
-         parser.add_option("-c", "--csvtimefile", dest="csvtimefile",
-                           help="write CSV format timeline file", metavar="FILE")
+    def setup_logging(self):
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-         parser.add_option("--no-csv-header",
-                          action="store_true", dest="no_csv_header", default=False,
-                          help="Suppresses the CSV header") 
-         
-         parser.add_option("-l", "--localtz",
-                           action="store_true", dest="localtz",
-                           help="report times using local timezone")
-     
-         parser.add_option("-d", "--debug",
-                           action="store_true", dest="debug",
-                           help="turn on debugging output")
-         
-         (self.options, args) = parser.parse_args()
-         
-     def open_files(self):
-          if (self.options.version == True):
-               print("Version is: %s" % (VERSION))
-               sys.exit()
+    def open_files(self):
+        try:
+            self.file_mft = open(self.options.filename, 'rb')
+        except IOError as e:
+            logging.error(f"Unable to open file: {self.options.filename}. Error: {e}")
+            sys.exit(1)
 
-          if self.options.filename == None:
-               print  ("-f <filename> required.")
-               sys.exit()
+        if self.options.output:
+            try:
+                output_file = open(self.options.output, 'w', newline='')
+                self.file_csv = csv.writer(output_file, dialect=csv.excel, quoting=csv.QUOTE_ALL)
+            except IOError as e:
+                logging.error(f"Unable to open file: {self.options.output}. Error: {e}")
+                sys.exit(1)
 
-          if self.options.output == None and self.options.bodyfile == None and self.options.csvtimefile == None:
-               print  ("-o <filename> or -b <filename> or -c <filename> required.")
-               sys.exit()
+        if self.options.bodyfile:
+            try:
+                self.file_body = open(self.options.bodyfile, 'w')
+            except IOError as e:
+                logging.error(f"Unable to open file: {self.options.bodyfile}. Error: {e}")
+                sys.exit(1)
 
-          try:
-               self.file_mft = open(self.options.filename, 'rb')
-          except:
-               print  ("Unable to open file: %s" % self.options.filename)
-               sys.exit()
+        if self.options.csvtimefile:
+            try:
+                self.file_csv_time = open(self.options.csvtimefile, 'w')
+            except IOError as e:
+                logging.error(f"Unable to open file: {self.options.csvtimefile}. Error: {e}")
+                sys.exit(1)
 
-          if self.options.output != None:
-               try:
-                    self.file_csv = csv.writer(open(self.options.output, 'wb'), dialect=csv.excel,quoting=1)
-               except (IOError, TypeError):
-                    print  ("Unable to open file: %s" % self.options.output)
-                    sys.exit()
+    def process_mft_file(self):
+        analyzer = MFTAnalyzer(self.options)
+        analyzer.process_mft_file(self.file_mft)
+        self.mft = analyzer.mft
+        self.folders = analyzer.folders
 
-          if self.options.bodyfile != None:
-               try:
-                    self.file_body = open(self.options.bodyfile, 'w')
-               except:
-                    print  ("Unable to open file: %s" % self.options.bodyfile)
-                    sys.exit()
+    def print_records(self):
+        for i, record in self.mft.items():
+            if self.file_csv:
+                self.file_csv.writerow(mft_to_csv(record, False))
+            if self.file_csv_time:
+                self.file_csv_time.write(mft_to_l2t(record))
+            if self.file_body:
+                self.file_body.write(mft_to_body(record, self.options.bodyfull, self.options.bodystd))
 
-          if self.options.csvtimefile != None:
-               try:
-                    self.file_csv_time = open(self.options.csvtimefile, 'w')
-               except (IOError, TypeError):
-                    print  ("Unable to open file: %s" % self.options.csvtimefile)
-                    sys.exit()
+    def close_files(self):
+        for file in [self.file_mft, self.file_body, self.file_csv_time]:
+            if file:
+                file.close()
+        if self.file_csv:
+            self.file_csv.close()
 
-     def process_mft_file(self):
-
-          # reset the file reading (since we did some pre-processing)
-          # mft_file.seek(0)
-
-          self.num_records = 0
-          if self.options.output is not None and not self.options.no_csv_header:
-               self.file_csv.writerow(mft.mft_to_csv('',True))
-
-          # 1024 is valid for current version of Windows but should really get this value from somewhere
-          raw_record = self.file_mft.read(1024)
-
-          while raw_record != "":
-
-               record = {}
-               record = mft.parse_record(raw_record, self.options)
-               if self.options.debug: print (record)
-               self.mft[self.num_records] = record
-
-               self.num_records = self.num_records + 1
-
-    #           if self.num_records > 10000:
-    #               break
-
-               raw_record = self.file_mft.read(1024)
-
-          self.gen_filepaths()
-
-     def print_records(self):
-          for i in self.mft:
-               if self.options.output != None:
-                    self.file_csv.writerow(mft.mft_to_csv(self.mft[i], False))
-               if self.options.csvtimefile != None:
-                    self.file_csv_time.write(mft.mft_to_l2t(self.mft[i]))
-               if self.options.bodyfile != None:
-                    self.file_body.write(mft.mft_to_body(self.mft[i], self.options.bodyfull, self.options.bodystd))
-
-
-
-
-     def gen_filepaths(self):
-
-          for i in self.mft:
-
-  #            if filename starts with / or ORPHAN, we're done.
-  #            else get filename of parent, add it to ours, and we're done.
-
-               # If we've not already calculated the full path ....
-               if (self.mft[i]['filename']) == '':
-
-                    if ( self.mft[i]['fncnt'] > 0 ):
-                         self.get_folder_path(i)
-                         # self.mft[i]['filename'] = self.mft[i]['filename'] + '/' + self.mft[i]['fn',self.mft[i]['fncnt']-1]['name']
-                         # self.mft[i]['filename'] = self.mft[i]['filename'].replace('//','/')
-                         if self.debug: print  ("Filename (with path): %s" % self.mft[i]['filename'])
-                    else:
-                         self.mft[i]['filename'] == 'NoFNRecord'
-
-
+    def run(self):
+        try:
+            self.open_files()
+            self.process_mft_file()
+            self.print_records()
+        finally:
+            self.close_files()

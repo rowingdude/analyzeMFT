51dec6fa9dcb6af096201c38796aadb50fd9bf23
Benjamin Cance <bjc@tdx.li>
2024-08-16 09:21:57 -0400

    Wrap the rest of the operations in a try/except block. Throw an error if time <0, and if the timestamp is invalid.

--- analyze_mft/windows_time.py
+++ analyze_mft/windows_time.py
@@ -19,24 +19,32 @@ class WindowsTime:
         self._parse_time()
 
     def _parse_time(self):
+
         if (self.low == 0) and (self.high == 0):
             self.dtstr = "Never"
             return
         
-        self.unixtime = self.get_unix_time()
-              
         try:
+            self.unixtime = self.get_unix_time()
+            
+            if self.unixtime < 0:
+                raise ValueError("Negative Unix time calculated")
+            
             if self.localtz:
                 self.dt = datetime.fromtimestamp(self.unixtime).astimezone()
+
             else:
                 self.dt = datetime.fromtimestamp(self.unixtime, tz=timezone.utc)
             
             self.dtstr = self.dt.isoformat()
           
-        except Exception as e:
+        except (ValueError, OverflowError) as e:
+
             self.dtstr = f"Invalid timestamp: {e}"
             self.unixtime = 0
 
     def get_unix_time(self):
+
         t = float(self.high) * 2**32 + self.low
+        
         return (t * 1e-7 - 11644473600)

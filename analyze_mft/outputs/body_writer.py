7281bd7b930671119e28e56064aee3922d9e1a5d
Benjamin Cance <bjc@tdx.li>
2024-08-26 13:28:49 -0400

    body_writer - add async to each function, add string format validater.

--- analyze_mft/outputs/body_writer.py
+++ analyze_mft/outputs/body_writer.py
@@ -1,35 +1,37 @@
-# This format: MD5|name|inode|mode_as_string|UID|GID|size|atime|mtime|ctime|crtime
+import os
+from typing import Dict, Any
 
-class BodyfileWriter:
-    def __init__(self, output_file):
-        self.output_file = output_file
+class BodyFileWriter:
+    def __init__(self, options: Any, file_handler: Any):
+        self.options = options
+        self.file_handler = file_handler
 
-    def _format_mode_as_string(self, mode):
-        # Convert file mode to a string representation like 'rwxr-xr-x'
-        is_dir = 'd' if os.path.isdir(mode) else '-'
+    async def write_records(self, mft: Dict[int, Dict[str, Any]]):
+        for record in mft.values():
+            body_record = await self._format_record(record)
+            await self.file_handler.write_body(body_record + '\n')
+
+    async def _format_record(self, record: Dict[str, Any]) -> str:
+        md5 = record.get('md5', '0')
+        name = record.get('filename', '')
+        if self.options.bodyfull:
+            name = record.get('full_path', name)
+        inode = str(record.get('recordnum', '0'))
+        mode = await self._format_mode_as_string(record.get('mode', 0))
+        uid = str(record.get('uid', '0'))
+        gid = str(record.get('gid', '0'))
+        size = str(record.get('size', '0'))
+        atime = str(record.get('si', {}).get('atime', '0'))
+        mtime = str(record.get('si', {}).get('mtime', '0'))
+        ctime = str(record.get('si', {}).get('ctime', '0'))
+        crtime = str(record.get('si', {}).get('crtime', '0'))
+
+        return f"{md5}|{name}|{inode}|{mode}|{uid}|{gid}|{size}|{atime}|{mtime}|{ctime}|{crtime}"
+
+    async def _format_mode_as_string(self, mode: int) -> str:
+        is_dir = 'd' if os.path.isdir(str(mode)) else '-'
         perm = ''
         for who in ('USR', 'GRP', 'OTH'):
             for what in ('R', 'W', 'X'):
                 perm += (who + what) in os.popen(f'stat -c %A {mode}').read()[-4:] and what.lower() or '-'
         return is_dir + perm
-
-    def _format_record(self, record):
-        md5 = record.get('md5', '')
-        name = record.get('filename', '')
-        inode = record.get('inode', '0')
-        mode = self._format_mode_as_string(record.get('mode', 0))
-        uid = str(record.get('uid', '0'))
-        gid = str(record.get('gid', '0'))
-        size = str(record.get('size', '0'))
-        atime = str(record.get('atime', '0'))
-        mtime = str(record.get('mtime', '0'))
-        ctime = str(record.get('ctime', '0'))
-        crtime = str(record.get('crtime', '0'))
-
-        return f"{md5}|{name}|{inode}|{mode}|{uid}|{gid}|{size}|{atime}|{mtime}|{ctime}|{crtime}"
-
-    def write_records(self, records):
-        with open(self.output_file, 'w') as bf:
-            for record in records:
-                formatted_record = self._format_record(record)
-                bf.write(formatted_record + '\n')

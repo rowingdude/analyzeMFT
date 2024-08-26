59f2044d98baf3a3885d57a4a881aa1be841fdc0
Benjamin Cance <bjc@tdx.li>
2024-08-26 13:29:33 -0400

    csv_timeline - finish prepare_csv_timeline_record and convert all functions to async

--- analyze_mft/outputs/csv_timeline.py
+++ analyze_mft/outputs/csv_timeline.py
@@ -1,13 +1,30 @@
+from typing import Dict, Any
+from io import StringIO
+import csv
+
 class CSVTimelineWriter:
-    def __init__(self, options, file_handler):
+    def __init__(self, options: Any, file_handler: Any):
         self.options = options
         self.file_handler = file_handler
 
-    async def write_records(self, mft):
+    async def write_records(self, mft: Dict[int, Dict[str, Any]]):
         for record in mft.values():
-            csv_timeline_record = self._prepare_csv_timeline_record(record)
-            await self.file_handler.write_csvtime(csv_timeline_record)
+            csv_timeline_record = await self._prepare_csv_timeline_record(record)
+            output = StringIO()
+            csv.writer(output).writerow(csv_timeline_record)
+            await self.file_handler.write_csv_time(output.getvalue())
 
-    def _prepare_csv_timeline_record(self, record):
-        
-        pass
+    async def _prepare_csv_timeline_record(self, record: Dict[str, Any]) -> List[str]:
+        timeline_record = [
+            record.get('filename', ''),
+            str(record.get('recordnum', '')),
+            record.get('si', {}).get('crtime', ''),
+            record.get('si', {}).get('mtime', ''),
+            record.get('si', {}).get('atime', ''),
+            record.get('si', {}).get('ctime', ''),
+            record.get('fn', {}).get('crtime', ''),
+            record.get('fn', {}).get('mtime', ''),
+            record.get('fn', {}).get('atime', ''),
+            record.get('fn', {}).get('ctime', '')
+        ]
+        return timeline_record

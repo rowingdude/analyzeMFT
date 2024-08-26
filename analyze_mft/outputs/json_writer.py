7a9aea3557f7ed5280687a80bd026ae4b90c9026
Benjamin Cance <bjc@tdx.li>
2024-08-26 13:27:54 -0400

    json_writer - add async to each function

--- analyze_mft/outputs/json_writer.py
+++ analyze_mft/outputs/json_writer.py
@@ -1,14 +1,16 @@
 import json
+from typing import Dict, Any
+
 class JSONWriter:
-    def __init__(self, options, file_handler):
+    def __init__(self, options: Any, file_handler: Any):
         self.options = options
         self.file_handler = file_handler
         self.json_data = []
 
-    def write_json_record(self, record):
+    async def write_json_record(self, record: Dict[str, Any]):
         self.json_data.append(record)
 
-    def write_json_file(self):
+    async def write_json_file(self):
         if self.options.jsonfile:
-            with open(self.options.jsonfile, 'w', encoding='utf-8') as json_file:
-                json.dump(self.json_data, json_file, indent=4)
+            json_string = json.dumps(self.json_data, indent=4)
+            await self.file_handler.write_json(json_string)

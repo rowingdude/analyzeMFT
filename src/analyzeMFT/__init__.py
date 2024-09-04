8c2eb5b5eddca025365daeaaea10ff72dd7f8435
Benjamin Cance <bjc@tdx.li>
2024-09-04 15:00:19 -0400

    Fix up init.py and add a path modifier to the initialization, ensuring we're consistently referenced.

--- src/analyzeMFT/__init__.py
+++ src/analyzeMFT/__init__.py
@@ -1,6 +1,18 @@
 from .windows_time import WindowsTime
 from .mft_record import MftRecord
 from .mft_analyzer import MftAnalyzer
-from .constants import VERSION
+from .file_writers import FileWriters
+from .constants import VERSION, CSV_HEADER
+from .cli import main
 
-__all__ = ['WindowsTime', 'MftRecord', 'MftAnalyzer', 'VERSION']
+__all__ = [
+    'WindowsTime',
+    'MftRecord',
+    'MftAnalyzer',
+    'FileWriters',
+    'VERSION',
+    'CSV_HEADER',
+    'main'
+]
+
+__version__ = VERSION

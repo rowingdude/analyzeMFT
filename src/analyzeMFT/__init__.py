9cfd6e78c318f03df77313216c8376380526028f
Benjamin Cance <bjc@tdx.li>
2024-09-05 08:34:15 -0400

    Bug hunting asyncio distributable files

--- src/analyzeMFT/__init__.py
+++ src/analyzeMFT/__init__.py
@@ -3,7 +3,10 @@ from .mft_record import MftRecord
 from .mft_analyzer import MftAnalyzer
 from .file_writers import FileWriters
 from .constants import VERSION, CSV_HEADER
-from .cli import main
+from .cli import main as cli_main
+
+def main():
+    asyncio.run(cli_main())
 
 __all__ = [
     'WindowsTime',
